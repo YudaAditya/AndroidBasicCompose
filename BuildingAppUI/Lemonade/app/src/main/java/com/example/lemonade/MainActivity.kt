4b10189dd68880b3db0a2b37eda9f02f01894c62
Yuda Aditya <38045460+YudaAditya@users.noreply.github.com>
2023-01-02 08:33:55 +0700

    Lemon Squeeze update

---
+++ BuildingAppUI/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+//    onImageClick:()-> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = textLabelResourceId),
+            fontSize = 18.sp
+        )
+        Image(
+            painter = painterResource(id = drawableResourceId),
+            contentDescription = stringResource(id = contentDescriptionResourceId),
+            modifier = Modifier
+                .wrapContentSize()
+//                .clickable(
+//                    onClick = onImageClick
+//                )
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun LemonadeApp() {
+    LemonTextAndImage(
+        textLabelResourceId = R.string.lemon_tree,
+        drawableResourceId = R.drawable.lemon_tree,
+        contentDescriptionResourceId = R.string.lemon_tree_description,
+//        onImageClick =
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LemonadeTheme {
+        LemonadeApp()
+    }
+}
